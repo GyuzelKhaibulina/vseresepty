8f8765850eb6607b353b8748b0d17e74e2da4a47
Guzel Khaibulina <guzel.khaibulina@emotorwerks.com>
2023-09-30 19:13:25 +0300

    send ourlook

--- controllers/services/SendOutlook.js
+++ controllers/services/SendOutlook.js
@@ -2,18 +2,16 @@ import nodemailer from 'nodemailer';
 import { OUTLOOK_HOST, OUTLOOK_USER, OUTLOOK_PASSW, SMTP_PORT_TLS } from '../../const/const.js';
 
 class SendOutlook {
-    #transporter = null;
+    transporter = null;
     constructor() {
-        this.#transporter = this.#getTransporter();
+        this.transporter = this.getTransporter();
     }
 
-    #getTransporter () {
+    getTransporter () {
         return nodemailer.createTransport({
             host: OUTLOOK_HOST, 
             secureConnection: false, 
             port: SMTP_PORT_TLS,
-            // logger: true,
-            // debu: true,
             auth: {
                 user: OUTLOOK_USER,
                 pass: OUTLOOK_PASSW
@@ -25,8 +23,8 @@ class SendOutlook {
     }
     async send (reciever, message, subject, html) {
         try {
-            const info = await this.#transporter.sendMail ({
-                from: '"VsRecepti" <vsrecepti@outlook.com>',
+            const info = await this.transporter.sendMail ({
+                from: "'VsRecepti' <vsrecepti@outlook.com>",
                 to: reciever,
                 subject: subject,
                 text: message,
